165a1d0ea7231394b777161ba485cf8c1ef43887
Jared-Glenn <jared_glenn@yahoo.com>
2022-11-26 11:02:15 -0700

    Initial Commit

---
+++ Days 51-75/070_blog_deployment/security.py
@@ -0,0 +1,12 @@
+SMTP_GMAIL = "smtp.gmail.com"
+
+gmail_email1 = "test.testington39@gmail.com"
+gmail_password1 = "gtozhiyqoypcnodc"
+
+gmail_email2 = "test.testington.jr39@gmail.com"
+gmail_password2 = "owlpqyhzdqsbfjur"
+
+
+yahoo_email = "test.testington39@yahoo.com"
+yahoo_password = "" #No app passwords enabled.
+SMTP_YAHOO = "smtp.mail.yahoo.com"
